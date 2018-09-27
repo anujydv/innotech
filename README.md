bd3f526ef59de07af6ce5104004c968de279294e
Anuj Yadav <anujy647@gmail.com>
2018-09-27 12:52:32 +0530

    web view

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProjectBlockchain
-Decentralized and Social
---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('main');
+});
+router.get('/clothing', function(req, res, next) {
+    res.render('clothing');
+});
+router.get('/food', function(req, res, next) {
+    res.render('food');
+});
+router.get('/shelter', function(req, res, next) {
+    res.render('shelter');
+});
+router.get('/water', function(req, res, next) {
+    res.render('water');
+});
+router.get('/medicalCare', function(req, res, next) {
+    res.render('medicalCare');
+});
+router.get('/sanitation', function(req, res, next) {
+    res.render('sanitation');
+});
+router.get('/rescue', function(req, res, next) {
+    res.render('rescue');
+});
+module.exports = router;
