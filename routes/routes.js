d7be68cd2e9b756fa493eb0a1b88d51b96ed02b1
ashudeo4 <ashudeo4@gmail.com>
2018-10-10 14:41:55 +0530

    map added on rescue web

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Map = require('../server/model/camplocation');
+
+var getcampdata = function(req) {
+    // var st = 'helpavailable'.concat('.', page);
+    Map.find({
+        'helpavailable.water': 1
+    }, function(err, doc) {
+        // var doc1 = JSON.stringify(doc);
+        console.log(doc1);
+        // return doc;
+    });
+};
 
 // rescue page routes
 router.get('/', function(req, res, next) {
@@ -15,7 +27,12 @@ router.get('/shelter', function(req, res, next) {
     res.render('shelter');
 });
 router.get('/water', function(req, res, next) {
-    res.render('water');
+    Map.find({
+        'helpavailable.water': 1
+    }, function(err, doc) {
+        doc = JSON.stringify(doc);
+        res.render('water', { data: doc });
+    });
 });
 router.get('/medicalCare', function(req, res, next) {
     res.render('medicalCare');
@@ -31,22 +48,33 @@ router.get('/login', function(req, res, next) {
 });
 
 // camp dashboard routes
-router.get('/map', function (req, res, next) {
+router.get('/map', function(req, res, next) {
     res.render('map/demo.html');
 });
-router.get('/camplogin', function (req, res, next) {
+router.get('/camplogin', function(req, res, next) {
     res.render('login', {
         verified: 1
     });
 });
-router.get('/das-map',(req,res,next)=>{
+router.get('/das-map', (req, res, next) => {
     res.render('adminmap.html');
 });
-router.get('/dashboard', function (req, res, next) {
+router.get('/dashboard', function(req, res, next) {
     res.render('admin.html');
 });
-router.get('/addcamp', function (req, res, next) {
+router.get('/addcamp', function(req, res, next) {
     res.render('register');
 });
+
+var getcampdata = function(req) {
+    // var st = 'helpavailable'.concat('.', page);
+    Map.find({
+        'helpavailable.water': 1
+    }, function(err, doc) {
+        // var doc1 = JSON.stringify(doc);
+        // console.log(doc1);
+        return doc;
+    });
+};
 
 module.exports = router;
